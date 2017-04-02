1353dc9812934a289da663b652cbda8c5476fc03
HypeUFO <michael.maio22@gmail.com>
2017-04-02 15:52:32 -0700

    copy original repo

---
+++ test/karma.config.js
@@ -0,0 +1,33 @@
+var webpackConfig = require('../webpack.config.dev')
+webpackConfig.devtool = 'inline-source-map'
+
+module.exports = function (config) {
+  config.set({
+    browsers: process.env.TRAVIS ? [ 'ChromeTravis' ] : [ 'Chrome', 'Firefox' ],
+    singleRun: true,
+    frameworks: [ 'mocha', 'chai' ],
+    files: [
+      'karma.webpack.js'
+    ],
+    preprocessors: {
+      'karma.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+    reporters: [ 'mocha' ],
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    },
+    client: {
+      mocha: {
+        timeout: 60000
+      }
+    },
+    browserNoActivityTimeout: 60000,
+    customLaunchers: {
+      ChromeTravis: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
+  })
+}
